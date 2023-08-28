1838eeed46b2a3ce32af6a3ae847ee3a8860c280
RAJ <raju78@gmail.com>
2023-08-28 18:56:36 +0530

    ra

--- assign.py
+++ assign.py
@@ -1,51 +1,37 @@
-class Employee:
-    def _init_(self, emp_id, name, age, salary):
-        self.emp_id = emp_id
-        self.name = name
-        self.age = age
-        self.salary = salary
+# Employee data
+employee_data = [
+    {"Employee ID": "161E90", "Name": "Raman", "Age": 41, "Salary": 56000},
+    {"Employee ID": "161F91", "Name": "Himadri", "Age": 38, "Salary": 67500},
+    {"Employee ID": "161F99", "Name": "Jaya", "Age": 51, "Salary": 82100},
+    {"Employee ID": "171E20", "Name": "Tejas", "Age": 30, "Salary": 55000},
+    {"Employee ID": "171G30", "Name": "Ajay", "Age": 45, "Salary": 44000},
+]
 
-class EmployeeTable:
-    def _init_(self):
-        self.employees = []
+def print_employee_data(data):
+    print("{:<12} {:<10} {:<4} {:<10}".format("Employee ID", "Name", "Age", "Salary"))
+    print("="*40)
+    for employee in data:
+        print("{:<12} {:<10} {:<4} {:<10}".format(
+            employee["Employee ID"], employee["Name"], employee["Age"], employee["Salary"])
+        )
 
-    def add_employee(self, employee):
-        self.employees.append(employee)
+def sort_employee_data(data, key):
+    return sorted(data, key=lambda x: x[key])
 
-    def sort_by_age(self):
-        self.employees.sort(key=lambda emp: emp.age)
-
-    def sort_by_name(self):
-        self.employees.sort(key=lambda emp: emp.name)
-
-    def sort_by_salary(self):
-        self.employees.sort(key=lambda emp: emp.salary)
-
-    def display(self):
-        for emp in self.employees:
-            print(f"Employee ID: {emp.emp_id}, Name: {emp.name}, Age: {emp.age}, Salary: {emp.salary}")
-
-if __name__ == "_main_":
-    employee_table = EmployeeTable()
-
-    employee_table.add_employee(Employee("161E90", "Raman", 41, 56000))
-    employee_table.add_employee(Employee("161F91", "Himadri", 38, 67500))
-    employee_table.add_employee(Employee("161F99", "Jaya", 51, 82100))
-    employee_table.add_employee(Employee("171E20", "Tejas", 30, 55000))
-    employee_table.add_employee(Employee("171G30", "Ajay", 45, 44000))
-
-    print("Choose sorting parameter:")
+if __name__ == "__main__":
+    print("Sorting Options:")
     print("1. Age\n2. Name\n3. Salary")
-    choice = int(input("Enter your choice: "))
+    sorting_option = int(input("Choose a sorting parameter (1/2/3): "))
 
-    if choice == 1:
-        employee_table.sort_by_age()
-    elif choice == 2:
-        employee_table.sort_by_name()
-    elif choice == 3:
-        employee_table.sort_by_salary()
+    if sorting_option == 1:
+        sorted_data = sort_employee_data(employee_data, "Age")
+    elif sorting_option == 2:
+        sorted_data = sort_employee_data(employee_data, "Name")
+    elif sorting_option == 3:
+        sorted_data = sort_employee_data(employee_data, "Salary")
     else:
-        print("Invalid choice!")
+        print("Invalid sorting option selected")
+        exit()
 
-    print("\nSorted Employee Table:")
-    employee_table.display()
+    print("\nSorted Employee Data:")
+    print_employee_data(sorted_data)
